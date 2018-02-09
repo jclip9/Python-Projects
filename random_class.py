e57645b0c00c307c7f898c2f033faa22e5e3db58
Daltz333 <gamefollower26@gmail.com>
2018-02-08 20:34:08 -0500

    Initial Commit

---
+++ random_class.py
@@ -0,0 +1,33 @@
+import random
+def ask_b_g():
+	a= input("b or g?\n")
+	return (a)
+def pick_random(Race):
+	Race= [
+	'human',
+	'elf',
+	'ork',
+	'Dwarf',
+	'half elf',
+	'half ork',
+	'gnome',
+	'halfing',
+	]
+	return random.choice(Race)
+def pick_random(Class)
+	Class= [
+	'fighter',
+	'druid',
+	'barbaran',
+	'bard',
+	'wizard',
+	'ranger',
+	'paladin',
+	'sorcorer',
+	'monk',
+	]
+	return random.choice(Class)
+	
+print(b[0])
+print(g[2])
+print("pick_random") 
